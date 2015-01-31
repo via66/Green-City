d240284451377d2346f9053e852fb2fad7401ad0
walter-cavinaw <w.cavinaw@gmail.com>
2015-01-31 14:35:06 -0800

    generic project name

--- DjangoUnchained/settings.py
+++ DjangoUnchained/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'FoodFinder',
+    'project_name',
 )
 
 MIDDLEWARE_CLASSES = (
