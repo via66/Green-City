54617a1524c2041a76064e42b4d840f120d4d39c
walter-cavinaw <w.cavinaw@gmail.com>
2015-01-30 17:52:36 -0800

    Adding basic structure for Django 1.7 Project including all files/settings for Heroku Deployment

---
+++ DjangoUnchained/settings.py
@@ -0,0 +1,114 @@
+"""
+Django settings for DjangoUnchained project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+import dj_database_url
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'eb-sydakr9i06od9271e-53i)hqk)o55x368dz&!bt*q#zh625'
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'FoodFinder',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'DjangoUnchained.urls'
+
+WSGI_APPLICATION = 'DjangoUnchained.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+PRODUCTION = "PRODUCTION"
+DEV = "DEV"
+STAGING = "STAGING"
+
+if 'DEPLOYMENT_TYPE' in os.environ:
+    DEPLOYMENT = os.environ['DEPLOYMENT_TYPE'].upper()
+else:
+    DEPLOYMENT = DEV
+
+if DEPLOYMENT == PRODUCTION:
+    DEBUG = False
+    TEMPLATE_DEBUG = False
+    DATABASES['default'] = dj_database_url.config()
+
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # Allow all host headers
+    ALLOWED_HOSTS = ['*']
+
+    # Static asset configuration
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_URL = '/static/'
+
+    STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    )
+else:
+    DEBUG = True
+    TEMPLATE_DEBUG = True
+    STATIC_URL = '/static/'
+
+    TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'templates'),
+    )
