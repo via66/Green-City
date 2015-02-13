19607124d9ee3ba89b9c5260787832d582cfbbbd
walter-cavinaw <w.cavinaw@gmail.com>
2015-02-13 13:01:20 -0800

    changed project name and added initial green project model

--- DjangoUnchained/settings.py
+++ DjangoUnchained/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'project_name',
+    'GreenCity',
 )
 
 MIDDLEWARE_CLASSES = (
---
+++ GreenCity/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# TODO: Green Project Model: MAPID,NAME,CATEGORY1,CATEGORY2,ADDRESS,SHORT_DESCRIPTION,URL,URL2,URL3,LATITUDE,LONGITUDE
+class GreenProject(models.Model):
+    map_id = models.CharField(max_length=6)
+    name = models.CharField(max_length=100)
+    cat_1 = models.CharField(max_length=50)
+    cat_2 = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    description = models.CharField(max_field=200)
+    url_1 = models.URLField()
+    url_2 = models.URLField()
+    url_3 = models.URLField()
+    lat = models.FloatField()
+    lon = models.FloatField()
