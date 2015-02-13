5141fffae5fe8776a34e332e2fc0e5a27bc5e09e
walter-cavinaw <w.cavinaw@gmail.com>
2015-02-13 15:15:33 -0800

    added admin model for green project

--- GreenCity/admin.py
+++ GreenCity/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from models import GreenProject
 
 # Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    fieldsets = [
+        (None,               {'fields': ['name']}),
+        ('Description', {'fields': ['description']}),
+    ]
+
+admin.site.register(GreenProject, ProjectAdmin)
