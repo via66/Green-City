847aaf24c299f98835a76aa8c54234e85e1651db
Velina <b6i8@ugrad.cs.ubc.ca>
2015-03-05 15:16:02 -0800

    Display map

--- DjangoUnchained/urls.py
+++ DjangoUnchained/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'GreenCity.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+	url(r'^index/', 'GreenCity.views.map', name='map'),
     url(r'^admin/', include(admin.site.urls)),
 )
