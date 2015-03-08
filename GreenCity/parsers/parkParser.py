1710e17085a53e29be809f27e242c4aa935d0a01
walter-cavinaw <w.cavinaw@gmail.com>
2015-03-07 20:24:37 -0800

    changed the way that icons are loaded. also changed the format of the parkParser to fit pep 8 better.

--- GreenCity/parsers/parkParser.py
+++ GreenCity/parsers/parkParser.py
@@ -1,62 +1,54 @@
+import csv
+import urllib2
+from GreenCity.models import Park
+
 
 def parsePark(url):
-    import csv
-    import urllib2
-    import time
-    from datetime import datetime
-    from GreenCity.models import Park
     f = urllib2.urlopen(url)
     reader = csv.reader(f)
     reader.next()
     for row in reader:
-		newPark = Park(
-			name = unicode(row[1],"ISO-8859-1"),
-			streetNumber = unicode(row[3],"ISO-8859-1"),
-			streetName = unicode(row[4],"ISO-8859-1"),
-			longitude = googleMapDest(unicode(row[7],"ISO-8859-1"),"longitude"),
-			latitude = googleMapDest(unicode(row[7],"ISO-8859-1"),"latitude"),
-			hectare = float(row[8]),
-			neighbourhoodName = unicode(row[9],"ISO-8859-1"),
-			neighbourhoodURL = unicode(row[10],"ISO-8859-1"),
-			washrooms = stringToBoolean(unicode(row[14],"ISO-8859-1"))
-		)
-		try:
-			newPark.save()
-		except:
-			print "Could not save your data, check if you entered a valid list of parks."
-	
+        newPark = Park(
+            name=unicode(row[1], "ISO-8859-1"),
+            streetNumber=unicode(row[3], "ISO-8859-1"),
+            streetName=unicode(row[4], "ISO-8859-1"),
+            longitude=googleMapDest(unicode(row[7], "ISO-8859-1"), "longitude"),
+            latitude=googleMapDest(unicode(row[7], "ISO-8859-1"), "latitude"),
+            hectare=float(row[8]),
+            neighbourhoodName=unicode(row[9], "ISO-8859-1"),
+            neighbourhoodURL=unicode(row[10], "ISO-8859-1"),
+            washrooms=stringToBoolean(unicode(row[14], "ISO-8859-1"))
+        )
+        try:
+            newPark.save()
+        except:
+            print "Could not save your data, check if you entered a valid list of parks."
+
+
 def googleMapDest(longitude_and_latitude, coordinate_type):
-	separate_coordinates = longitude_and_latitude.split(",")
-	if coordinate_type == "latitude":
-		return separate_coordinates[0]
-	elif coordinate_type == "longitude":
-		return separate_coordinates[1]
+    separate_coordinates = longitude_and_latitude.split(",")
+    if coordinate_type == "latitude":
+        return separate_coordinates[0]
+    elif coordinate_type == "longitude":
+        return separate_coordinates[1]
+
 
 def stringToBoolean(string):
-	if "Y" in string:
-		return True
-	elif "N" in string:
-		return False
+    if "Y" in string:
+        return True
+    elif "N" in string:
+        return False
 
 
 # run from "manage.py shell"
 # execfile('path to the file')
 #
+if __name__ == "__main__" or __name__ == '__builtin__':
+    import os
+    import django
 
-if __name__ == "__main__":
-	
-	import os
-	import django
-	import csv
-	import urllib2
-	import time
-	from datetime import datetime
-	from GreenCity.models import Park
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoUnchained.settings")
+    django.setup()
 
-	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoUnchained.settings")
-	django.setup()
-	
-	from django.conf import settings
-
-	parsePark('ftp://webftp.vancouver.ca/OpenData/csv/parks.csv')
+    parsePark('ftp://webftp.vancouver.ca/OpenData/csv/parks.csv')
 
