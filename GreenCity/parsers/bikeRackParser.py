87824078bf382c53fb02962d2303ca2e2690262d
walter-cavinaw <w.cavinaw@gmail.com>
2015-03-09 19:03:31 -0700

    changing the geolocation calls. Looked at doing bulk_create, but couldn't get it to work for inherited classes

--- GreenCity/parsers/bikeRackParser.py
+++ GreenCity/parsers/bikeRackParser.py
@@ -1,91 +1,71 @@
+import csv
+import urllib2
+from GreenCity.models import BikeRack
+from geopy.geocoders import Nominatim
+from decimal import *
+
 
 def parseBikeRack(url):
-	from django.conf import settings
-	import csv
-	import urllib2
-	import time
-	from datetime import datetime
-	from GreenCity.models import BikeRack
-	from django.core.exceptions import ValidationError
-	from geopy.geocoders import Nominatim
-	# from decimal import *
+    f = urllib2.urlopen(url)
+    reader = csv.reader(f)
+    reader.next()
+    parse_bike_rack_file(reader)
+    f.close()
 
-	f = urllib2.urlopen(url)
-	reader = csv.reader(f)
-	reader.next()
 
-	newBikeRackList = []
-	for row in reader:
-		completeAddress = generateName(unicode(row[0],"ISO-8859-1"),unicode(row[1],"ISO-8859-1"))
-		newName = completeAddress
-		newLongitude = 0.0 #getGeoLocation(completeAddress,"longitude")
-		newLatitude = 0.0 #getGeoLocation(completeAddress,"latitude")
-		newStreetSide = unicode(row[2],"ISO-8859-1")
-		newStreetNumber = unicode(row[0],"ISO-8859-1")
-		newStreetName = unicode(row[1],"ISO-8859-1")
-		newNumberOfRacks = int(unicode(row[5],"ISO-8859-1"))
+def parse_bike_rack_file(reader):
+    for row in reader:
+        print row
+        completeAddress = generateName(unicode(row[0], "ISO-8859-1"), unicode(row[1], "ISO-8859-1"))
+        newName = completeAddress
+        newLongitude = 0.0  # getGeoLocation(completeAddress,"longitude")
+        newLatitude = 0.0  # getGeoLocation(completeAddress,"latitude")
+        newStreetSide = unicode(row[2], "ISO-8859-1")
+        newStreetNumber = unicode(row[0], "ISO-8859-1")
+        newStreetName = unicode(row[1], "ISO-8859-1")
+        newNumberOfRacks = int(unicode(row[5], "ISO-8859-1"))
 
-		newBikeRack = BikeRack(
-			name = newName,
-			longitude = newLongitude,
-			latitude = newLatitude,
-			streetSide = newStreetSide,
-			streetNumber = newStreetNumber,
-			streetName = newStreetName,
-			numberOfRacks = newNumberOfRacks
-		)
-		
-		try:
-			newBikeRack.save()
-			newBikeRackList.append(newBikeRack.id)
-		except:
-			print "Could not save your data, check if you entered a valid list of bike racks."
-			pass
-	f.close()
-	print "Starting to get the geolocation of bike racks."
-	for bikeRackID in newBikeRackList:
-		bikeRack = BikeRack.objects.get(id=bikeRackID)
-		print "Bike rack "+str(bikeRackID)+"."
-		bikeRack.longitude = getGeoLocation(bikeRack.completeAddress,"longitude")
-		bikeRack.latitude = getGeoLocation(bikeRack.completeAddress,"latitude")
-		bikeRack.save()
+        newBikeRack = BikeRack(
+            name=newName,
+            longitude=newLongitude,
+            latitude=newLatitude,
+            streetSide=newStreetSide,
+            streetNumber=newStreetNumber,
+            streetName=newStreetName,
+            numberOfRacks=newNumberOfRacks
+        )
+        try:
+            newBikeRack.save()
+        except:
+            print "could not add, duplicate"
 
-def generateName(streetNumber,streetName):
-	return streetNumber + " " + streetName
 
-def getGeoLocation(completeAddress,longitude_and_latitude):
-	geolocator = Nominatim()
-	completeAddress = completeAddress+" "+"Vancouver"
-	location = geolocator.geocode(completeAddress, timeout=500)
+def get_lat_lon_from_address():
+    for bikeRack in BikeRack.objects.all():
+        bikeRack.latitude, bikeRack.longitude = getGeoLocation(bikeRack.completeAddress)
+        bikeRack.save()
 
-	if longitude_and_latitude == "latitude":
-		# print Decimal(unicode(location.latitude))
-		return Decimal(unicode(location.latitude))
-	elif longitude_and_latitude == "longitude":
-		# print Decimal(unicode(location.longitude))
-		return Decimal(unicode(location.longitude))
+
+def generateName(streetNumber, streetName):
+    return streetNumber + " " + streetName
+
+
+def getGeoLocation(completeAddress):
+    geolocator = Nominatim()
+    completeAddress = completeAddress + " " + "Vancouver"
+    location = geolocator.geocode(completeAddress, timeout=500)
+    return Decimal(unicode(location.latitude)), Decimal(unicode(location.longitude))
 
 # run from "manage.py shell"
 # execfile('path to the file')
 #
 
-if __name__ == "__main__":
-	
-	import os
-	import django
+if __name__ == "__main__" or __name__ == '__builtin__':
+    import os
+    import django
 
-	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoUnchained.settings")
-	django.setup()
-	
-	from django.conf import settings
-	import csv
-	import urllib2
-	import time
-	from datetime import datetime
-	from GreenCity.models import BikeRack
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoUnchained.settings")
+    django.setup()
 
-	from geopy.geocoders import Nominatim
-	from decimal import *
-
-	parseBikeRack('ftp://webftp.vancouver.ca/opendata/bike_rack/BikeRackData.csv')
+    parseBikeRack('ftp://webftp.vancouver.ca/opendata/bike_rack/BikeRackData.csv')
 
