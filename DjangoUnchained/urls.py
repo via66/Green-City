3d5040d227bb2030829471f4485eaf52da3c2e39
Phillip M <Pmisiurak@Gmail.com>
2015-03-19 15:58:09 -0700

    login w/o registration db (superuser only for now)

--- DjangoUnchained/urls.py
+++ DjangoUnchained/urls.py
@@ -2,10 +2,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-                       # Examples:
-                       url(r'^$', 'GreenCity.views.home', name='home'),
-                       # url(r'^blog/', include('blog.urls')),
                        url(r'^admin/', include(admin.site.urls)),
-                       url(r'^filter/', 'GreenCity.views.filter', name='filter'),
-
+                       url(r'^$', 'GreenCity.views.home', name='home'),
+                       # url(r'^index/', 'GreenCity.views.map', name='map'),
+                       # user auth urls
+                       url(r'^accounts/login/$', 'GreenCity.views.login'),
+                       url(r'^accounts/auth/$', 'GreenCity.views.auth_view'),
+                       url(r'^accounts/logout/$', 'GreenCity.views.logout'),
+                       url(r'^accounts/loggedin/$', 'GreenCity.views.loggedin'),
+                       url(r'^accounts/invalid/$', 'GreenCity.views.invalid_login'),
 )
--- GreenCity/views.py
+++ GreenCity/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+
 from django.http import HttpResponse
 from django.template import RequestContext, loader, Context
 from GreenCity.models import Feature
 from django.core import serializers
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.core.context_processors import csrf
 import json
 
 # Create your views here.
@@ -17,3 +21,30 @@ def filter(request):
 	# temporarily returning all Features
 	data = serializers.serialize("json", Feature.objects.all())	
 	return HttpResponse(data, content_type="application/json")
+
+def login(request):
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('login.html', c)
+
+def auth_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('/accounts/loggedin')
+    else:
+        return HttpResponseRedirect('/accounts/invalid')
+
+def loggedin(request):
+    return render_to_response('loggedin.html', { 'full_name': request.user.username})
+
+def invalid_login(request):
+    return render_to_response('invalid_login.html')
+
+def logout(request):
+    auth.logout(request)
+    return render_to_response('logout.html')
+
