daacb64e9339e3aa74d0915990dde50b608c9236
Marina <mahmlima@gmail.com>
2015-03-20 14:00:33 -0700

    Little change on login page
    Now dealing with wrong login data

--- GreenCity/views.py
+++ GreenCity/views.py
@@ -57,6 +57,7 @@ def register(request):
 
 def user_login(request):
 	context_dict = {}
+	user_not_found = False
 	if request.method == 'POST':
 		username = request.POST.get('username')
 		password = request.POST.get('password')
@@ -70,8 +71,8 @@ def user_login(request):
 			else:
 				return HttpResponse("Your account is disabled.")
 		else:
-			print "Invalid login details: {0}, {1}".format(username, password)
-			return HttpResponse("Invalid login details supplied.")
+			user_not_found = True
+			return render(request, 'login.html', {'user_not_found':user_not_found})
 	else:
 		return render(request, 'GreenCity/login.html', {})
 
