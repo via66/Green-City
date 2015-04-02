53a947f7548e2a79c6ede3e745e9d3867a4ca169
Phillip M <Pmisiurak@Gmail.com>
2015-04-01 19:33:23 -0700

    fixed same name error

--- GreenCity/parsers/electricVehicleChargingStationParser.py
+++ GreenCity/parsers/electricVehicleChargingStationParser.py
@@ -7,9 +7,10 @@ def parseElectricVehicleChargingStation(url):
     f = urllib2.urlopen(url)
     reader = csv.reader(f)
     reader.next()
+    count = 1
     for row in reader:
         newChargingStation = ElectricVehicleChargingStation(
-            name=unicode(row[3], "ISO-8859-1"),
+            name=unicode(row[3] + ',' + str(count), "ISO-8859-1"),
             longitude=unicode(row[1], "ISO-8859-1"),
             latitude=unicode(row[0], "ISO-8859-1"),
             lotOperator=unicode(row[2], "ISO-8859-1"),
@@ -17,6 +18,7 @@ def parseElectricVehicleChargingStation(url):
         )
         try:
             newChargingStation.save()
+            count = count + 1
         except:
             print "Could not save your data, check if you entered a valid list of charging stations."
 
